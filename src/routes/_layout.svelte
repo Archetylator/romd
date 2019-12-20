f786b76e6416e67c5270db2f954c3c39dec26689
Marcin Syngajewski <archetelecynacja@gmail.com>
2019-12-20 20:55:00 +0200

    Add files to /docs

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,10 +1,10 @@
 <script context="module">
-  import { waitLocale } from 'svelte-i18n'
+  //import { waitLocale } from 'svelte-i18n'
 
-  export async function preload() {
+  //export async function preload() {
     // awaits for the loading of the 'en-US' and 'en' dictionaries
-    return waitLocale()
-  }
+  //  return waitLocale()
+  //}
 </script>
 
 <script>
